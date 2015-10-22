8b21cecca6c64f55f4b797bee5bac212553e930f
John Capps <jcapps@cloudhop.io>
2015-10-22 12:29:25 -0400

    inital commit

---
+++ cookbooks/ch-base/metadata.rb
@@ -0,0 +1,7 @@
+name 'ch-base'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures ch-base'
+long_description 'Installs/Configures ch-base'
+version '0.1.0'
---
+++ cookbooks/ch-base/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: ch-base
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
